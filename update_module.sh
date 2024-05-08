fbd8f809ba83936a410385aaebbdc6748e3eeb5e
Randell <piszmogcode@gmail.com>
2024-05-07 20:13:32 -0600

    Add script to change module name

---
+++ update_module.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <new module name>"
+	exit 1
+fi
+
+new_module=$1
+
+find . -type d -name .git -prune -o -type f -exec sed -i '' -e "s/my-new-mod/${new_module}/g" {} \;
