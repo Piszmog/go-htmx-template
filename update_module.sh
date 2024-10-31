65d31d54168178e51fe846022e4d043eae08fad7
Randell Callahan <piszmogcode@gmail.com>
2024-10-30 22:07:45 -0600

    Update update_module.sh

--- update_module.sh
+++ update_module.sh
@@ -7,4 +7,4 @@ fi
 
 new_module=$1
 
-find . -type d -name .git -prune -o -type f -exec sed -i '' -e "s/my-new-mod/${new_module}/g" {} \;
+find . -type d -name .git -prune -o -type f -exec sed -i '' -e "s|go-htmx-template|${new_module}|g" {} \;
