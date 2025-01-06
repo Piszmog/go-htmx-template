9cc8524cf2585483b84f3e4301db2d79ec23f5a7
Randell <piszmogcode@gmail.com>
2025-01-06 15:46:18 -0700

    Add a convenience method to add method to routes

--- server/router/router.go
+++ server/router/router.go
@@ -17,8 +17,12 @@ func New(logger *slog.Logger, database db.Database) http.Handler {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/assets/", middleware.CacheMiddleware(http.FileServer(http.FS(dist.AssetsDir))))
-	mux.HandleFunc("/", h.Home)
+	mux.Handle(newPath(http.MethodGet, "/assets/"), middleware.CacheMiddleware(http.FileServer(http.FS(dist.AssetsDir))))
+	mux.HandleFunc(newPath(http.MethodGet, "/"), h.Home)
 
 	return middleware.NewLoggingMiddleware(logger, mux)
+}
+
+func newPath(method string, path string) string {
+	return method + " " + path
 }
